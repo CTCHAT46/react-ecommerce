8f598fa648c201074b7bd10a6fe6d6acfa7dd7ca
CTCHAT46 <132136945+CTCHAT46@users.noreply.github.com>
2023-05-19 03:46:44 +0700

    updated

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -86,8 +86,8 @@ function footer() {
             </div>
             <div className="bg-gray-100">
                 <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
-                    <p className="text-gray-500 text-sm text-center sm:text-left">© 2020 Tailblocks —
-                        <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank">@knyttneve</a>
+                    <p className="text-gray-500 text-sm text-center sm:text-left">© 2023
+                        <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank">CT</a>
                     </p>
                     <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
                         <a className="text-gray-500">
